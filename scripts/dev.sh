8157009c03a91b4be41ba0ce682f33d1bd461fbe
Huan LI <zixia@zixia.net>
2020-03-10 14:05:32 +0800

    add dev script

---
+++ scripts/dev.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+set -x
+
+#  --privileged \
+
+docker run \
+  -ti --rm \
+  --name windows \
+  -e USER_PASSWD='Passw0rd' \
+  -e VNC_GEOMETRY=1600x900 \
+  -e VNC_PASSWD='Passw0rd' \
+  -p 80:80 \
+  -p 22:22 \
+  --entrypoint /usr/bin/sh \
+  windows
